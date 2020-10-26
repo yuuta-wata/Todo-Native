24b6086db476a54809fb7e27a7c2b4dcbccd2269
yuuta <watanabe@unlog.co.jp>
2020-10-26 12:49:25 +0900

    Code refactoring

--- app/screen/HomeScreen.tsx
+++ app/screen/HomeScreen.tsx
@@ -1,62 +1,20 @@
-import React, { FC, useEffect, useLayoutEffect } from 'react'
+import React, { FC, useEffect } from 'react'
 import { SafeAreaView, StyleSheet, View, Dimensions } from 'react-native'
 import { useDispatch } from 'react-redux'
-import { useNavigation, useRoute } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native'
 
-import {
-  HomeRouteProps,
-  HomeNavigationProps
-} from '../navigation/MainStack/type'
+import { HomeNavigationProps } from '../navigation/MainStack/type'
 import { FetchTaskListAction } from '../redux/modules/task/actions'
 import AddButton from '../components/home/AddButton'
 import TaskList from '../components/home/TaskList'
-import HamburgerButton from '../components/home/HamburgerButton'
 
 const HomeScreen: FC = () => {
   const dispatch = useDispatch()
   const navigation = useNavigation<HomeNavigationProps<'HomeScreen'>>()
-  const route = useRoute<HomeRouteProps<'HomeScreen'>>()
 
   const { height, width } = Dimensions.get('window')
   // AddButtonSize
   const addButtonSize = height * 0.08
-  // HambrugerSize
-  const hambrugerSize = height * 0.04
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerTitle: 'ホーム',
-      headerTitleStyle: {
-        color: '#EBEBEB',
-        fontSize: height * 0.03
-      },
-      headerLeft: () => (
-        <HamburgerButton
-          style={{
-            width: hambrugerSize,
-            height: hambrugerSize
-          }}
-          hamburgerStyle={{
-            width: hambrugerSize * 0.8,
-            height: hambrugerSize * 0.1,
-            borderRadius: hambrugerSize * 0.8 * 2
-          }}
-          onPress={() => {
-            route.params && route.params.props.navigation.openDrawer()
-          }}
-        />
-      ),
-      headerLeftContainerStyle: {
-        marginLeft: height * 0.02
-      },
-      headerStyle: {
-        borderBottomWidth: 1,
-        borderBottomColor: '#EBEBEB',
-        backgroundColor: '#2B6187'
-      }
-    })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [navigation])
 
   useEffect(() => {
     dispatch(FetchTaskListAction())
